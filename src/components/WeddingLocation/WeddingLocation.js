526f91a0c21e68864b46a8ebddf137dca6b09ce4
Tikesh24 <tikeshverma2494@gmail.com>
2022-05-01 20:24:25 +0530

    Changed date for reception

--- src/components/WeddingLocation/WeddingLocation.js
+++ src/components/WeddingLocation/WeddingLocation.js
@@ -12,18 +12,18 @@ function WeddingLocation() {
                     <div>
                         <h1>Ceremony</h1>
                         <p>Wednesday, 11, May, 2022</p>
-                        <h4>Haldi &diams; Mehendi &diams; Baraat Prasthan <a href="https://goo.gl/maps/z74Jjox4t8o6yeyn6" target="_blank" title="Go to map"><FaMapMarkerAlt /></a></h4>
+                        <h4>Haldi &diams; Mehendi &diams; Baraat Prasthan <a href="https://goo.gl/maps/z74Jjox4t8o6yeyn6" target="_blank" title="Thakur Palace | Go to map"><FaMapMarkerAlt /></a></h4>
                         <div className={styles.brline} />
                     </div>
                     <div>
                         <h1>Reception</h1>
-                        <p>Thursday, 11, May, 2022</p>
-                        <h4>Reception <a href="https://goo.gl/maps/QgnJzo37Ss5MKYgZ6" target="_blank" title="Go to map"><FaMapMarkerAlt /></a></h4>
+                        <p>12:00 PM - 4:00 PM, Thursday, 12, May, 2022</p>
+                        <h4>Reception <a href="https://goo.gl/maps/z74Jjox4t8o6yeyn6" target="_blank" title="Thakur Palace | Go to map"><FaMapMarkerAlt /></a></h4>
                         <div className={styles.brline} />
                     </div>
                     <div>
                         <h1>Location</h1>
-                        <h4>Thakur Palace  <a href="https://goo.gl/maps/z74Jjox4t8o6yeyn6" target="_blank" title="Go to map"><FaMapMarkerAlt /></a></h4>
+                        <h4>Thakur Palace  <a href="https://goo.gl/maps/z74Jjox4t8o6yeyn6" target="_blank" title="Thakur Palace | Go to map"><FaMapMarkerAlt /></a></h4>
                         <p>Professor Colony, Raipur, Chhattisgarh 492001</p>
                     </div>
                 </div>
